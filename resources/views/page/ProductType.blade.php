3ff8ae037be74f0ad3faefcf3b9df34f26b48976
Tùng Xuân <tungcan2000@gmail.com>
2020-05-12 09:34:28 +0700

    better

--- resources/views/page/ProductType.blade.php
+++ resources/views/page/ProductType.blade.php
@@ -107,7 +107,6 @@
 	<script src="source/assets/dest/js/wow.min.js"></script>
 	<!--customjs-->
 	<script src="source/assets/dest/js/custom2.js"></script>
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 @endsection
